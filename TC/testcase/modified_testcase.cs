89aea821605a378ac74c14cc397053a9573a340e
argan719 <64789601+argan719@users.noreply.github.com>
2024-11-11 10:26:04 +0900

    Upload Example TC and Excel File

---
+++ TC/testcase/modified_testcase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class TestCases
+{
+    public void ClPlTmpLvTestCase_001()
+    {
+        int temp = 25;
+        Console.WriteLine("Tmp test case with temperature: " + temp);
+    }
+
+    public void ClPlTmpLvTestCase_002()
+    {
+        bool isCl = true;
+        Console.WriteLine("Cl test case: " + isCl);
+    }
+
+    public void ClPlTmpLvTestCase_003()
+    {
+        int level = 3;
+        Console.WriteLine("Lv test case with level: " + level);
+    }
+
+    public void ClPlTmpLvTestCase_004()
+    {
+        string poolType = "Swimming Pl";
+        Console.WriteLine("Pl test case with pool type: " + poolType);
+    }
+
+    public void ClPlTmpLvTestCase_005()
+    {
+        int coolLv = 5;
+        Console.WriteLine("Cl level test case with cool level: " + coolLv);
+    }
+
+    public void ClPlTmpLvTestCase_006()
+    {
+        double tempInPl = 28.5;
+        Console.WriteLine("Tmp in pool test case with temperature: " + tempInPl);
+    }
+
+    public void ClPlTmpLvTestCase_007()
+    {
+        int level = 4;
+        string coolStatus = "Very Cl";
+        Console.WriteLine("Lv cool test case with level and status: " + level + ", " + coolStatus);
+    }
+
+    public void ClPlTmpLvTestCase_008()
+    {
+        double poolTmp = 30.0;
+        Console.WriteLine("Pl temp test case with pool temperature: " + poolTmp);
+    }
+
+    public void ClPlTmpLvTestCase_009()
+    {
+        int temp = 15;
+        bool isCl = temp < 20;
+        Console.WriteLine("Tmp cool test case with temp: " + temp + ", isCl: " + isCl);
+    }
+
+    public void ClPlTmpLvTestCase_010()
+    {
+        string pool = "Cl Pl";
+        Console.WriteLine("Cl pool test case with pool: " + pool);
+    }
+}
---
+++ TC/testcase/testcase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class TestCases
+{
+    public void CoolPoolTempLevelTestCase_007()
+    {
+        int temp = 25;
+        Console.WriteLine("Temp test case with temperature: " + temp);
+    }
+
+    public void CoolPoolTempLevelTestCase_002()
+    {
+        bool isCool = true;
+        Console.WriteLine("Cool test case: " + isCool);
+    }
+
+    public void CoolPoolTempLevelTestCase_004()
+    {
+        int level = 3;
+        Console.WriteLine("Level test case with level: " + level);
+    }
+
+    public void CoolPoolTempLevelTestCase_009()
+    {
+        string poolType = "Swimming Pool";
+        Console.WriteLine("Pool test case with pool type: " + poolType);
+    }
+
+    public void CoolPoolTempLevelTestCase_005()
+    {
+        int coolLevel = 5;
+        Console.WriteLine("Cool level test case with cool level: " + coolLevel);
+    }
+
+    public void CoolPoolTempLevelTestCase_001()
+    {
+        double tempInPool = 28.5;
+        Console.WriteLine("Temp in pool test case with temperature: " + tempInPool);
+    }
+
+    public void CoolPoolTempLevelTestCase_010()
+    {
+        int level = 4;
+        string coolStatus = "Very Cool";
+        Console.WriteLine("Level cool test case with level and status: " + level + ", " + coolStatus);
+    }
+
+    public void CoolPoolTempLevelTestCase_003()
+    {
+        double poolTemp = 30.0;
+        Console.WriteLine("Pool temp test case with pool temperature: " + poolTemp);
+    }
+
+    public void CoolPoolTempLevelTestCase_008()
+    {
+        int temp = 15;
+        bool isCool = temp < 20;
+        Console.WriteLine("Temp cool test case with temp: " + temp + ", isCool: " + isCool);
+    }
+
+    public void CoolPoolTempLevelTestCase_006()
+    {
+        string pool = "Cool Pool";
+        Console.WriteLine("Cool pool test case with pool: " + pool);
+    }
+}
